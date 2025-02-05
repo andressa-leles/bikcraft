91fb4c81043c027ec1e63685f23415d83258fdd9
Andressa Leles <leles.andressa@gmail.com>
2025-02-05 16:25:23 -0300

    items orcamento -JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//ativar links do Menu
+
 const links = document.querySelectorAll('.header-menu a')
 
 function ativarLink(link){
@@ -12,3 +14,16 @@ function ativarLink(link){
 }
 
 links.forEach(ativarLink)
+
+//Ativar Items do Orçamento
+
+const parametros = new URLSearchParams(location.search)
+
+function ativarProduto(parametro){
+    const elemento = document.getElementById(parametro)
+    if(elemento){
+        elemento.checked = true
+    }   
+}
+
+parametros.forEach(ativarProduto)
